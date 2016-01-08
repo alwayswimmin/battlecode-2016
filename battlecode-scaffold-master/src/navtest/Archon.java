abd5ce14f21167a19904b5f24b6a6369be8a4dd5
Samuel Hsiang <samuel.c.hsiang@gmail.com>
2016-01-07 22:54:49 -0500

    look in navtest for bugging nav. stuff will change; i will code my own bugging algo together with map analysis, etc. soon later

--- battlecode-scaffold-master/src/navtest/Archon.java
+++ battlecode-scaffold-master/src/navtest/Archon.java
@@ -5,16 +5,22 @@ import battlecode.common.*;
 public class Archon extends Bot {
 	public static void run(RobotController _rc) throws GameActionException {
 		Bot.init(_rc);
+		init();
 		while(true) {
 			action();
 		}
 	}
 	private static void init() throws GameActionException {
 		// things that run for the first time
-
+		personalHQ = new MapLocation(155, 279);
 	}
+	private static int scoutsBuilt = 0;
 	private static void action() throws GameActionException {
 		// take my turn
+		myLocation = rc.getLocation();
+		if (rc.isCoreReady()) {
+			Nav.goTo(personalHQ); // no avoid yet...
+		}
 		Clock.yield();
 	}
 }
