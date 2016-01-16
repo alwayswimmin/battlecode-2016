12b5672efc0a737da3df1d300fbec2134281a440
Samuel Hsiang <samuel.c.hsiang@gmail.com>
2016-01-15 20:39:13 -0500

    improved soldier attack

---
+++ battlecode-scaffold-master/src/attacktest/Nav.java
@@ -0,0 +1,195 @@
+package attacktest;
+
+import battlecode.common.*;
+
+interface SafetyPolicy {
+	public boolean safe(MapLocation loc);
+}
+
+// avoid none
+class SPNone extends Bot implements SafetyPolicy {
+	public boolean safe(MapLocation loc) {
+		return true;
+	}
+}
+
+// avoid all
+class SPAll extends Bot implements SafetyPolicy {
+	RobotInfo[] enemies;
+
+	public SPAll(RobotInfo[] _enemies) {
+		enemies = _enemies;
+	}
+
+	public SPAll() {
+		enemies = new RobotInfo[0];
+	}
+
+	public boolean safe(MapLocation loc) {
+		for (int i = enemies.length; --i >= 0; ) {
+			if (enemies[i].type.attackRadiusSquared >= loc.distanceSquaredTo(enemies[i].location)) return false;
+		}
+		return true;
+	}
+}
+
+public class Nav extends Bot {
+
+	private static MapLocation dest;
+	private static MapLocation start;
+	private static SafetyPolicy policy;
+	private static boolean obsFlag; // true means obstacle detected, false otherwise
+	private static boolean wallOnRight; // true means wall on right hand side in bug
+
+	private static Direction toDest;
+	private static Direction dir;
+
+	private static void init(MapLocation _dest, SafetyPolicy _policy) throws GameActionException {
+		dest = _dest;
+		start = myLocation;
+		policy = _policy;
+		obsFlag = false;
+		wallOnRight = true;
+		toDest = myLocation.directionTo(dest);
+		dir = toDest;
+	}
+
+	private static boolean move(Direction dir) throws GameActionException {
+		rc.move(dir);
+		return true;
+	}
+
+	private static boolean canMove(Direction dir) {
+		return rc.canMove(dir) && policy.safe(myLocation.add(dir));
+	}
+	// move directly or 45 degrees off. return true if worked.
+	private static boolean tryMoveDirect() throws GameActionException {
+		if (canMove(toDest)) {
+			move(toDest);
+			dir = toDest;
+			return true;
+		}
+
+		Direction dirLeft = toDest.rotateLeft();
+		Direction dirRight = toDest.rotateRight();
+		if (myLocation.add(dirLeft).distanceSquaredTo(dest) < myLocation.add(dirRight).distanceSquaredTo(dest)) {
+			if (canMove(dirLeft)) {
+				move(dirLeft);
+				dir = dirLeft;
+				return true;
+			}
+			if (canMove(dirRight)) {
+				move(dirRight);
+				dir = dirRight;
+				return true;
+			}
+		} else {
+			if (canMove(dirRight)) {
+				move(dirRight);
+				dir = dirRight;
+				return true;
+			}
+			if (canMove(dirLeft)) {
+				move(dirLeft);
+				dir = dirLeft;
+				return true;
+			}
+		}
+
+		double adjRubble = rc.senseRubble(rc.getLocation().add(toDest));
+
+		if (adjRubble >= 50 && adjRubble < 200) {
+			MapLocation locAfterMove = rc.getLocation().add(toDest);
+			Direction[] nextMoves = {toDest.rotateLeft(), toDest, toDest.rotateRight()};
+
+			if(rc.getType() != RobotType.TTM) {
+				boolean y = false;
+
+				for (int i = 0; i < 3; ++i)
+					for (int j = 0; j < 3; ++j) {
+						if ((dest.equals(locAfterMove.add(nextMoves[i])) || rc.senseRubble(locAfterMove.add(nextMoves[i])) < 50) && rc.onTheMap(locAfterMove.add(nextMoves[i])))
+							y = true;
+						if ((dest.equals(locAfterMove.add(nextMoves[i]).add(nextMoves[j])) || rc.senseRubble(locAfterMove.add(nextMoves[i]).add(nextMoves[j])) < 50) && rc.onTheMap(locAfterMove.add(nextMoves[i]).add(nextMoves[j])))
+							y = true;
+					}
+
+				if (y == true && rc.onTheMap(rc.getLocation().add(toDest))) {
+					rc.clearRubble(toDest);
+					return true;
+				}
+			}
+		}
+		return false;
+	}
+
+	private static void chooseWallDirection() throws GameActionException {
+		Direction leftDir = dir.rotateLeft();
+		for(int i = 3; --i >= 0;) {
+			if(!canMove(leftDir)) {
+				leftDir = leftDir.rotateLeft();
+			}
+			else {
+				break;
+			}
+		}
+		Direction rightDir = dir.rotateRight();
+		for(int i = 3; --i >= 0;) {
+			if(!canMove(rightDir)) {
+				rightDir = rightDir.rotateRight();
+			}
+			else {
+				break;
+			}
+		}
+		wallOnRight = dest.distanceSquaredTo(myLocation.add(leftDir)) < dest.distanceSquaredTo(myLocation.add(rightDir));
+		dir = wallOnRight ? leftDir : rightDir;
+	}
+
+	private static void followBoundary() throws GameActionException {
+		dir = wallOnRight ? dir.rotateRight() : dir.rotateLeft();
+		for(int i = 8; --i >= 0;) {
+			if(canMove(dir)) {
+				move(dir);
+				return;
+			}
+			dir = wallOnRight ? dir.rotateLeft() : dir.rotateRight();
+		}
+	}
+
+	public static void goTo(MapLocation _dest, SafetyPolicy _policy) throws GameActionException {
+		if(!_dest.equals(dest)) {
+			init(_dest, _policy);
+		}
+
+		if(myLocation.equals(dest)) {
+			return;
+		}
+
+		toDest = myLocation.directionTo(dest);
+
+		if(!tryMoveDirect()) {
+			if(!obsFlag) {
+				chooseWallDirection();
+			}
+			obsFlag = true;
+			followBoundary();
+		} else {
+			obsFlag = false;
+		}
+	}
+
+	public static void goTo(MapLocation _dest) throws GameActionException {
+		goTo(_dest, new SPNone());
+	}
+
+	public static MapLocation closestWall() throws GameActionException {
+		MapLocation[] visibleLocations = MapLocation.getAllMapLocationsWithinRadiusSq(myLocation, SIGHT_RANGE);
+		MapLocation toReturn = null;
+		for(int i = visibleLocations.length; --i >= 0; ) {
+			if(rc.onTheMap(visibleLocations[i]) && (toReturn == null || myLocation.distanceSquaredTo(visibleLocations[i]) < myLocation.distanceSquaredTo(toReturn))) {
+				toReturn = visibleLocations[i];
+			}
+		}
+		return toReturn;
+	}
+}
---
+++ battlecode-scaffold-master/src/attacktest/Soldier.java
@@ -0,0 +1,168 @@
+
+package attacktest;
+
+import battlecode.common.*;
+import java.util.*;
+
+public class Soldier extends Bot {
+	public static void run(RobotController _rc) throws GameActionException {
+		Bot.init(_rc);
+		init();
+		while(true) {
+			Radio.process();
+			myLocation = rc.getLocation();
+			action();
+			Radio.clear();
+			Clock.yield();
+		}
+	}
+	private static void init() throws GameActionException {
+		// things that run for the first time
+		personalHQ = rc.getLocation();
+		defendQueue = new LinkedList<Integer>();
+		moveQueue = new LinkedList<MapLocation>();
+		Radio.broadcastInitialStrategyRequest(10);
+	}
+
+	private static MapLocation defendLocation = null;
+	private static MapLocation attackLocation = null;
+	private static int turnsSinceLastAttack = 100;
+	private static void action() throws GameActionException {
+		// take my turn
+		processSignals();
+		RobotInfo[] enemiesWithinRange = rc.senseNearbyRobots(ATTACK_RANGE, enemyTeam);
+		RobotInfo[] zombiesWithinRange = rc.senseNearbyRobots(ATTACK_RANGE, Team.ZOMBIE);
+		if (enemiesWithinRange.length > 0) {
+			// Check if weapon is ready
+			if (rc.isWeaponReady()) {
+				rc.attackLocation(enemiesWithinRange[0].location);
+				turnsSinceLastAttack = 0;
+			}
+		} else if (zombiesWithinRange.length > 0) {
+			// Check if weapon is ready
+			if (rc.isWeaponReady()) {
+				rc.attackLocation(zombiesWithinRange[0].location);
+				turnsSinceLastAttack = 0;
+			}
+		}
+		switch(strategy) {
+			case -1:
+				int channel = Radio.getTuneCommand();
+				if(channel == 30) {
+					strategy = Radio.getStrategyAssignment();
+				}
+				break;
+			case 0:
+				break;
+			case 1:
+				if(turnsSinceLastAttack >= 2) {
+					moveSomewhere();
+				}
+				break;
+			default:
+				break;
+		}
+
+		if(rc.isCoreReady()) {
+			RobotInfo[] immediateHostile = rc.senseHostileRobots(myLocation, 4);
+			for(int i = immediateHostile.length; --i >= 0; ) {
+				if(immediateHostile[i].type == RobotType.STANDARDZOMBIE || immediateHostile[i].type == RobotType.BIGZOMBIE
+						|| immediateHostile[i].type == RobotType.FASTZOMBIE || immediateHostile[i].type == RobotType.GUARD) {
+					Nav.goTo(myLocation.add(immediateHostile[i].location.directionTo(myLocation)));
+					if(!rc.isCoreReady()) {
+						break;
+					}
+				}
+			}
+		}
+		if(rc.isCoreReady()) {
+			RobotInfo[] immediateHostile = rc.senseHostileRobots(myLocation, ATTACK_RANGE);
+			for(int i = immediateHostile.length; --i >= 0; ) {
+				if(immediateHostile[i].type == RobotType.ARCHON || immediateHostile[i].type == RobotType.ZOMBIEDEN
+						|| immediateHostile[i].type == RobotType.TTM || immediateHostile[i].type == RobotType.TURRET || immediateHostile[i].type == RobotType.SCOUT) {
+					Nav.goTo(immediateHostile[i].location);
+					if(!rc.isCoreReady()) {
+						break;
+					}
+				}
+			}
+		}
+
+				if(turnsSinceLastAttack >= 2) {
+		if (rc.isCoreReady()) {
+			int rot = (int)(Math.random() * 8);
+			Direction dirToMove = Direction.EAST;
+			for (int i = 0; i < rot; ++i)
+				dirToMove = dirToMove.rotateLeft();
+
+			for (int i = 0; i < 8; ++i) {
+				if (rc.canMove(dirToMove)) {
+					rc.move(dirToMove); break;
+				}
+
+				dirToMove = dirToMove.rotateLeft();
+			}
+			}
+		}
+		turnsSinceLastAttack++;
+	}
+
+	private static LinkedList<Integer> defendQueue;
+	private static LinkedList<MapLocation> moveQueue;
+	private static MapLocation[] teamLocations = new MapLocation[32001];
+	private static int[] teamMemberNeedsHelp = new int[32001]; // store what turn request was made
+
+	private static void processSignals() throws GameActionException {
+		IdAndMapLocation newDefend = null, newMove = null; int clearDefend = -1;
+		newDefend = Radio.getDefendLocation(); newMove = Radio.getMoveLocation(); clearDefend = Radio.getClearDefend();
+		IdAndMapLocation newHQ = Radio.getMoveCampLocation();
+		if(newHQ != null) {
+			personalHQ = newHQ.location;
+		}
+		while(newDefend != null) {
+			if(teamMemberNeedsHelp[newDefend.id] == 0) {
+				defendQueue.add(newDefend.id);
+				teamMemberNeedsHelp[newDefend.id] = rc.getRoundNum();
+			}
+			teamLocations[newDefend.id] = newDefend.location;
+			newDefend = Radio.getDefendLocation();
+		}
+		while(newMove != null) {
+			moveQueue.add(newMove.location);
+			newMove = Radio.getMoveLocation();
+		}
+		while(clearDefend != -1) {
+			teamMemberNeedsHelp[clearDefend] = 0;
+			clearDefend = Radio.getClearDefend();
+		}
+	}
+
+	private static void moveSomewhere() throws GameActionException {
+		if(strategy == 0) {
+			while(!defendQueue.isEmpty()) {
+				int next = defendQueue.element();
+				if(teamMemberNeedsHelp[next] > 0 && rc.getRoundNum() - teamMemberNeedsHelp[next] < 200) {
+					if(rc.isCoreReady()) {
+						Nav.goTo(teamLocations[next]);
+					}
+					return;
+				}
+				defendQueue.remove();
+			}
+		}
+		if(!moveQueue.isEmpty()) {
+			MapLocation next = moveQueue.element();
+			if(rc.isCoreReady()) {
+				Nav.goTo(next);
+			}
+			if(rc.canSense(next) && rc.senseRobotAtLocation(next) == null) {
+				moveQueue.remove();
+			}
+			return;
+		}
+        if(rc.isCoreReady()) {
+            Nav.goTo(personalHQ);
+            return;
+        }
+	}
+}
